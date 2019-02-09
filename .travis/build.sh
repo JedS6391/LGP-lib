e5b0f0abba84c4b4a83acaad317e00ddb8ae8746
Jed Simson <jed.simson@gmail.com>
2019-02-09 16:03:08 +1300

    Set up Travis CI.

---
+++ .travis/build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+chmod u+x gradlew
+
+./gradlew build --no-daemon --stacktrace --console=plain
---
+++ .travis/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+branch_name=$(echo $1 | sed -e 's/\//-/g')
+tag_name=$2
+now=$(date +%Y-%m-%d)
+version=$(gradle -q printVersion)
+release_branch_regex="^release.*$"
+feature_branch_regex="^feature.*$"
+
+echo "$branch_name"
+echo "$tag_name"
+
+if [[ "$branch_name" =~ $feature_branch_regex ]]; then
+    echo "Renaming build files for feature branch..."
+    mv build/libs/LGP-lib-"$version".jar build/libs/LGP-core-"$branch_name"-"$now".jar
+    ls build/libs
+elif [[ "$branch_name" =~ $release_branch_regex ]]; then
+    echo "Renaming build files for release branch..."
+    mv build/libs/LGP-lib-"$version".jar build/libs/LGP-core-"$tag_name"-"$now".jar
+    ls build/libs
+else
+    echo "Invalid deployment type"
+fi
